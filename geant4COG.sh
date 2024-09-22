6cda5476d1db0821e73a114ff50333c47cb69235
Keith Sloan <keith@sloan-home.co.uk>
2024-09-22 09:00:28 +0100

    scripts working

--- geant4COG.sh
+++ geant4COG.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
-#iexport Path=$PATH:.
-#export Path=$PATH:$(pwd).
 #echo $PATH
+# printCM is needed for awk script
 cd FC_exported_gdml
-touch ../geant4_failures.txt
+LOGFILE="../geant4_failures.txt"
 #cp ../vis.mac vis.mac
 #cp ../initInit.mac initInit.mac
 cp ../batch.mac batch.mac
-for f in *.gdml; do load_gdml_color $f -m=batch.mac >${f}.out; done
-retVal=$?
-if [ $retVal -ne 0 ]; then
-    echo $f "Failed " $retval >> ../geant4_failures.txt
-fi    
-for f in *.out; do gawk -f ../filter_load_gdml_CM.awk $f; done
+for f in *.gdml; do
+    load_gdml_color $f -printCM -m=batch.mac >${f}.out 2>&1;
+    retCode=$?
+    if [ $retCode -ne 0 ]; then
+        echo $f "Failed " $retCode >> $LOGFILE
+    fi
+done    
+for f in *.out; do 
+    gawk -f ../filter_load_gdml_CM.awk "$f" >"${f%}.txt"
+done
 exit
     
